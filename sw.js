664858b6cc546424e57873bc01e133b514f72ccd
Ifestiban <2014gilmario@gmail.com>
2026-03-14 20:27:53 -0300

    Update sw.js
    atualização do cache

--- sw.js
+++ sw.js
@@ -1,17 +1,43 @@
+const CACHE_NAME = "iadplan-agenda";
 
-self.addEventListener('install', event => {
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open('iadplan-agenda-v2').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './styles.css',
-      './app.js',
-      './manifest.json',
-      './icon-192.png',
-      './icon-512.png'
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
